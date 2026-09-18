2a93c41b4199fcc09210f7b7709636e852eb583b
Luis <markingcomic40@gmail.com>
2026-09-18 18:51:12 +0200

    chore: release v0.2.0

--- web/src/site.config.ts
+++ web/src/site.config.ts
@@ -2,7 +2,7 @@ export const site = {
   name: 'TypeSH',
   tagline: 'Practice typing like a true 10x engineer,',
   taglineJoke: 'Or typing prompts like a true vibe coder..? type sh B)',
-  version: '0.1.0', // in sync with Cargo.toml as of writing this
+  version: '0.2.0', // in sync with Cargo.toml as of writing this
   repo: 'https://github.com/Markingcomic40/type-sh',
   releases: 'https://github.com/Markingcomic40/type-sh/releases/latest',
   install: 'cargo install type-sh',
